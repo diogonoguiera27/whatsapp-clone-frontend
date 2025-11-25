4844af26505f14d2b7b4544be445c77c02914d92
diogonoguiera27 <diogoalvesnogueira3@gmail.com>
2025-11-25 09:11:15 -0300

    Configuração Incial Frontend

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold text-red-600">
+        Tailwind 4.1 funcionando! 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
